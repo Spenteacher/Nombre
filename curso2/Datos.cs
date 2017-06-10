93d210db53c03ccb0f9f16d706d39bf4264c566a
Spenteacher <samzaragoza.vcia@gmail.com>
2017-06-10 08:49:09 +0200

    Add files via upload

---
+++ curso2/Datos.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curso2
+{
+    class Datos
+    {
+
+        String nombre;
+
+        public void setNombre(String n)
+        {
+            nombre = n;
+
+        }
+        String Apellido1;
+
+
+        public void setApellido(String a)
+        {
+          Apellido1 = a;
+
+        }
+
+        public String getApellido1()
+        {
+            return Apellido1;
+        }
+
+
+        public String getNombre()
+        {
+            return nombre;
+        }
+
+        public void setApellido2(String e)
+        {
+            Apellido2 = e; 
+        }
+
+        String Apellido2;
+
+        public String getApellido2()
+        {
+            return Apellido2;
+        }
+
+        }
+    }
+
---
+++ curso2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curso2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Datos d = new Datos();
+
+        
+            Console.WriteLine("PROGRAMA DE RECOPILACION DE DATOS PARA EL ESTADO MAYOR DE LA REPUBLICA");
+
+            Console.WriteLine("Escriba su nombre");
+            //set nombre(String readline);
+            d.setNombre(Console.ReadLine());
+
+            Console.WriteLine("Su nombre capturado es : " + d.getNombre());
+
+            Datos a = new Datos();
+
+
+            Console.WriteLine("Escriba su primer apellido");
+            //set nombre(String readline);
+            a.setApellido(Console.ReadLine());
+
+            Console.WriteLine("Su apellido capturado es : " + a.getApellido1());
+
+            Datos e = new Datos();
+
+
+            Console.WriteLine("Escriba su segundo apellido");
+            //set nombre(String readline);
+            e.setApellido2(Console.ReadLine());
+
+            Console.WriteLine("Su segundo apellido capturado es : " + e.getApellido2());
+
+            Console.WriteLine("Su nombre completo es " + d.getNombre() + a.getApellido1() + e.getApellido2());
+            Console.WriteLine("Enviando al servidor estoesunabroma.com");
+            Console.WriteLine("Broma finalizada");
+           
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
